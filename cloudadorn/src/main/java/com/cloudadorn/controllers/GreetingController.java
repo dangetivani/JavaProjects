0c9293666f685c7fa5a65442f3d4c7c4430d53f4
Dangeti <vani.a.dangeti@capgemini.com>
2020-02-15 20:51:31 -0500

    spring restful webservice - cloud adorn greeting

---
+++ cloudadorn/src/main/java/com/cloudadorn/controllers/GreetingController.java
@@ -0,0 +1,25 @@
+package com.cloudadorn.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cloudadorn.resources.Greeting;
+
+
+
+@RestController
+@RequestMapping("/cloudadorn")
+public class GreetingController {
+
+	private static final String template = "Cloudadorn - One stop site for learning cloud technologies, "+" Welcome %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+}
---
+++ cloudadorn/src/test/java/com/cloudadorn/mainapp/CloudadornApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudadorn.mainapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudadornApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
